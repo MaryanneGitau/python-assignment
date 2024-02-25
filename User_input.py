f1c63e5c85c07f88230932217dcdc1c0f0503b83
MaryanneGitau <maryannembeere@gmail.com>
2024-02-25 13:38:24 +0300

    user_input

---
+++ User_input.py
@@ -0,0 +1,13 @@
+# user_input.py
+
+# Ask the user for their name
+name = input("Please enter your name: ")
+
+# Ask the user for their age
+age = input("Please enter your age: ")
+
+# Ask the user for their location
+location = input("Please enter your location: ")
+
+# Print the collected information
+print(f"Hello, {name}! You are {age} years old and located in {location}.")
